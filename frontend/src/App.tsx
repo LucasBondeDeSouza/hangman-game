5710b5ff5931e96a6cdf4a4c7b18f1b8071b79ac
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-08-25 16:19:02 -0300

    atualização

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import words from './wordList.json'
+import HangmanDrawing from "./components/HangmanDrawing"
+import HangmanWord from "./components/HangmanWord"
+import Keyboard from "./components/Keyboard"
+
+const App = () => {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  return (
+    <div className="max-w-[800px] flex flex-col gap-[2rem] my-0 mx-auto items-center">
+      <p className="text-[2rem] text-center">Lose Win</p>
+
+      <HangmanDrawing />
+      <HangmanWord />
+      <div className="self-stretch">
+        <Keyboard />
+      </div>
+    </div>
+  )
+}
+
+export default App
